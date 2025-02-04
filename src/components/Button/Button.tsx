d6ae88be3760a10b7a6e967fa70c9d5ff4803c76
simonjaeggi <dev.simon.jaeggi@gmail.com>
2025-02-04 14:57:19 +0100

    added icon prop to button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,16 +4,17 @@ interface ButtonProps {
     label:string;
     bgColor?:string;
     clickhandler: () => void;
+    icon?:string;
 }
 
-function Button({label, bgColor='#488aec', clickhandler}:ButtonProps) {
+function Button({label, bgColor='#488aec', clickhandler, icon}:ButtonProps) {
     const buttonStyle = {
         backgroundColor: bgColor,
       };
-      
+
     return (
         <div>
-            <button style={buttonStyle} onClick={clickhandler}>{label}</button>
+            <button style={buttonStyle} onClick={clickhandler}>{label} {icon &&  <i className={icon}></i>}</button>
         </div>
     )
 }
