14c1a0e0ab6dd6e01957e5b2008a4f1ce4b9a77b
simonjaeggi <dev.simon.jaeggi@gmail.com>
2025-02-04 14:11:33 +0100

    rewrote form generation to be dynamic and added buttons for adding and deleting form sections

--- src/components/Form/WorkForm/WorkForm.tsx
+++ src/components/Form/WorkForm/WorkForm.tsx
@@ -1,27 +1,87 @@
-import "../Form.css"
-import Input from "../../Input/Input.tsx"
-import Button from "../../Button/Button.tsx"
-import FormTitle from "../FormTitle/FormTitle.tsx"
+import "../Form.css";
+import Input from "../../Input/Input.tsx";
+import Button from "../../Button/Button.tsx";
+import FormTitle from "../FormTitle/FormTitle.tsx";
 import { useState } from "react";
 
-
 function WorkForm() {
-  const [isEditable, setEditable] = useState(true);
+    const [isEditable, setEditable] = useState(true);
+    const [inputs, setInputs] = useState([{id: 0}]);
 
-  const buttonClickHandler = () => { setEditable(!isEditable) }
+    const saveButtonClickHandler = () => {
+        setEditable(!isEditable);
+    };
+    const addButtonClickHandler = () => {
+        setInputs((previousInputs) => [
+            ...previousInputs,
+            {id: inputs.length},
+        ]);
+    };
 
-  return (
-    <div className="form-section">
-      <FormTitle title="Work Experience"></FormTitle>
-      <div className="form-inputs">
-        <Input isDisabled={!isEditable} placeholder="Alphabet Inc" label="Company"></Input>
-        <Input isDisabled={!isEditable} placeholder="Senior Software Engineer" label="Position"></Input>
-        <Input isDisabled={!isEditable} label="Start Date" type="date"></Input>
-        <Input isDisabled={!isEditable} label="End Date" type="date"></Input>
-      </div>
-      <Button clickhandler={buttonClickHandler} label={isEditable ? "Save" : "Edit"} bgColor={isEditable ? undefined : "#D93934"}></Button>
-      </div>
-  )
+    const removeClickHandler = () => {
+        setInputs((previousInputs) => previousInputs.slice(0, -1));
+    };
+
+    return (
+        <div className="form-section">
+            <div className="flex-row space-between">
+                <FormTitle
+                    icon="ri-briefcase-fill"
+                    title="Work Experience"
+                ></FormTitle>
+
+                <div className="flex-row gap-1rem">
+                    {inputs.length > 1 && (
+                        <Button
+                            clickhandler={removeClickHandler}
+                            bgColor="#D93934"
+                            label="Remove last"
+                        ></Button>
+                    )}
+
+                    <Button
+                        clickhandler={addButtonClickHandler}
+                        bgColor="#9478C8"
+                        label="ADD"
+                    ></Button>
+                </div>
+            </div>
+            {inputs.map(( id ) => (
+                <div key={`input-wrapper-${id}`} className="form-inputs">
+                    <Input
+                        key={`company-${id}`}
+                        isDisabled={!isEditable}
+                        placeholder="Alphabet Inc"
+                        label="Company"
+                    />
+                    <Input
+                        key={`position-${id}`}
+                        isDisabled={!isEditable}
+                        placeholder="Senior Software Engineer"
+                        label="Position"
+                    />
+                    <Input
+                        key={`start-${id}`}
+                        isDisabled={!isEditable}
+                        label="Start Date"
+                        type="date"
+                    />
+                    <Input
+                        key={`end-${id}`}
+                        isDisabled={!isEditable}
+                        label="End Date"
+                        type="date"
+                    />
+                </div>
+            ))}
+
+            <Button
+                clickhandler={saveButtonClickHandler}
+                label={isEditable ? "Save" : "Edit"}
+                bgColor={isEditable ? undefined : "#D93934"}
+            ></Button>
+        </div>
+    );
 }
 
-export default WorkForm
+export default WorkForm;
