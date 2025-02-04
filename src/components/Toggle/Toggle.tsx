1f9186f7ba097bab9f243495b4f744009d316ef4
simonjaeggi <dev.simon.jaeggi@gmail.com>
2025-02-04 14:58:01 +0100

    created toggle component

---
+++ src/components/Toggle/Toggle.tsx
@@ -0,0 +1,14 @@
+import "./Toggle.css";
+
+function Toggle() {
+    return (
+        <>
+            <label className="switch">
+                <input type="checkbox"></input>
+                <span className="slider"></span>
+            </label>
+        </>
+    );
+}
+
+export default Toggle;
