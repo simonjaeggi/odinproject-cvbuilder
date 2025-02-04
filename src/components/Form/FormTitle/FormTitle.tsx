02fb390779a0d715c37f3fffc2c47513b20c2cf5
simonjaeggi <dev.simon.jaeggi@gmail.com>
2025-02-04 14:10:09 +0100

    added icon to formtitle as prop

--- src/components/Form/FormTitle/FormTitle.tsx
+++ src/components/Form/FormTitle/FormTitle.tsx
@@ -1,6 +1,11 @@
-import "./FormTitle.css"
-function FormTitle({ title }: { title: string }) {
-    return <span className="FormTitle">{title}</span>;
+import "./FormTitle.css";
+function FormTitle({ title, icon }: { title: string; icon: string }) {
+    return (
+        <div className="FormTitle flex-row gap-1rem">
+            <i className={icon}></i>
+            <span>{title}</span>
+        </div>
+    );
 }
 
 export default FormTitle;
