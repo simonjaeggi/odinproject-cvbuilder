324d11c6d4ebebc2c2379e97813c2f01554a8f81
simonjaeggi <dev.simon.jaeggi@gmail.com>
2025-02-04 16:58:31 +0100

    imported checkbox "until present" on experiences

--- src/components/Form/WorkForm/WorkForm.tsx
+++ src/components/Form/WorkForm/WorkForm.tsx
@@ -3,10 +3,12 @@ import Input from "../../Input/Input.tsx";
 import Button from "../../Button/Button.tsx";
 import FormTitle from "../FormTitle/FormTitle.tsx";
 import { useState } from "react";
+import Checkbox from "../../Checkbox/Checkbox.tsx";
 
 function WorkForm() {
     const [isEditable, setEditable] = useState(true);
     const [inputs, setInputs] = useState([{ id: 0 }]);
+
 
     const saveButtonClickHandler = () => {
         setEditable(!isEditable);
@@ -20,6 +22,12 @@ function WorkForm() {
 
     const removeClickHandler = () => {
         setInputs((previousInputs) => previousInputs.slice(0, -1));
+    };
+
+    const changeHandlerUntilPresent = (endDateInputKey:string) => {
+      console.log(endDateInputKey);
+      
+      // I want to do something with the input that has this key
     };
 
     return (
@@ -50,34 +58,48 @@ function WorkForm() {
                     </div>
                 )}
             </div>
-            {inputs.map(({id}) => (
-                <div key={`work-input-wrapper-${id}`} className="form-inputs">
-                    <Input
-                        key={`work-company-${id}`}
-                        isDisabled={!isEditable}
-                        placeholder="Alphabet Inc"
-                        label="Company"
-                    />
-                    <Input
-                        key={`work-position-${id}`}
-                        isDisabled={!isEditable}
-                        placeholder="Senior Software Engineer"
-                        label="Position"
-                    />
-                    <Input
-                        key={`work-start-${id}`}
-                        isDisabled={!isEditable}
-                        label="Start Date"
-                        type="date"
-                    />
-                    <Input
-                        key={`work-end-${id}`}
-                        isDisabled={!isEditable}
-                        label="End Date"
-                        type="date"
-                    />
-                </div>
-            ))}
+            {inputs.map(({ id }) => {
+                const endDateInputKey = `work-end-${id}`;
+                return (
+                    <div
+                        key={`work-input-wrapper-${id}`}
+                        className="form-inputs"
+                    >
+                        <Input
+                            key={`work-company-${id}`}
+                            isDisabled={!isEditable}
+                            placeholder="Alphabet Inc"
+                            label="Company"
+                        />
+                        <Input
+                            key={`work-position-${id}`}
+                            isDisabled={!isEditable}
+                            placeholder="Senior Software Engineer"
+                            label="Position"
+                        />
+                        <Input
+                            key={`work-start-${id}`}
+                            isDisabled={!isEditable}
+                            label="Start Date"
+                            type="date"
+                        />
+                        <Input
+                            key={endDateInputKey}
+                            isDisabled={!isEditable}
+                            label="End Date"
+                            type="date"
+                        />
+                        <Checkbox
+                            label="Until Present"
+                            isDisabled={!isEditable}
+                            // I dont really get this. Apparently I can skip giving the child the argument??
+                            onChangeHandler={() =>
+                                changeHandlerUntilPresent(endDateInputKey)
+                            }
+                        ></Checkbox>
+                    </div>
+                );
+            })}
 
             <Button
                 clickhandler={saveButtonClickHandler}
