132e55aa16a3b19b8a40b91d067e6db8e46f3bbf
simonjaeggi <dev.simon.jaeggi@gmail.com>
2025-02-06 17:36:09 +0100

    use external state to handle information

--- src/components/Form/InformationForm/InformationForm.tsx
+++ src/components/Form/InformationForm/InformationForm.tsx
@@ -1,15 +1,49 @@
 import "../Form.css";
-import "./InformationForm.css";
 import Input from "../../Input/Input.tsx";
 import Button from "../../Button/Button.tsx";
 import FormTitle from "../FormTitle/FormTitle.tsx";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-function InformationForm() {
+interface InformationFormProps {
+    info: Record<string, string>;
+    setInfo: React.Dispatch<React.SetStateAction<Record<string, string>>>;
+}
+
+function InformationForm({ info, setInfo }: InformationFormProps) {
     const [isEditable, setEditable] = useState(true);
 
+    const infoNameId = "info-name";
+    const infoSurnameId = "info-surname";
+    const infoEmailId = "info-email";
+    const infoPhoneId = "info-phone";
+
+    // ensure the state is always populated
+    useEffect(() => {
+        setInfo((prev) => ({
+            ...prev,
+            [infoNameId]: "",
+            [infoSurnameId]: "",
+            [infoEmailId]: "",
+            [infoPhoneId]: "",
+        }));
+    }, []);
+
+    const updatedInfo = {...info};
+
     const saveButtonClickHandler = () => {
+        if (isEditable) {
+            setInfo({...updatedInfo});
+        }
         setEditable(!isEditable);
+    };
+    const handleInputChange = ({
+        value,
+        id,
+    }: {
+        value: string;
+        id: string;
+    }) => {
+        updatedInfo[id] = value;
     };
 
     return (
@@ -18,28 +52,41 @@ function InformationForm() {
                 icon="ri-information-2-fill"
                 title="General Information"
             ></FormTitle>
-            <div className="form-inputs">
+
+            <div key={`info-input-wrapper`} className="form-inputs">
                 <Input
+                    key={infoNameId}
+                    id={infoNameId}
                     isDisabled={!isEditable}
                     placeholder="Bill"
                     label="Name"
+                    handleChange={handleInputChange}
                 ></Input>
                 <Input
+                    key={infoSurnameId}
+                    id={infoSurnameId}
                     isDisabled={!isEditable}
                     placeholder="Clinton"
                     label="Surname"
+                    handleChange={handleInputChange}
                 ></Input>
                 <Input
+                    key={infoEmailId}
+                    id={infoEmailId}
                     isDisabled={!isEditable}
                     placeholder="bill.clinton@gmail.com"
                     label="Email"
                     type="email"
+                    handleChange={handleInputChange}
                 ></Input>
                 <Input
+                    key={infoPhoneId}
+                    id={infoPhoneId}
                     isDisabled={!isEditable}
                     placeholder="+41 76 123 45 65"
                     label="Phone"
                     type="tel"
+                    handleChange={handleInputChange}
                 ></Input>
             </div>
             <Button
--- src/components/Form/Work/WorkForm.tsx
+++ src/components/Form/Work/WorkForm.tsx
@@ -1,25 +1,30 @@
-import { useState } from "react";
-import WorkInstance from "./WorkInstance";
+import WorkBlock from "./WorkBlock";
 import Button from "../../Button/Button";
 import FormTitle from "../FormTitle/FormTitle";
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
-export default function WorkForm() {
-    const [inputs, setInputs] = useState([
-        { id: uuidv4()},
-    ]);
+interface WorkFormProps {
+    work: Record<string, Record<string, string>>;
+    setWork: React.Dispatch<
+        React.SetStateAction<Record<string, Record<string, string>>>
+    >;
+}
+
+export default function WorkForm({ work, setWork }: WorkFormProps) {
     const addButtonClickHandler = () => {
-        setInputs((previousInputs) => [
-            ...previousInputs,
-            { id: uuidv4()}
-        ]);
+        setWork((prev) => ({
+            ...prev,
+            [uuidv4()]: {},
+        }));
     };
 
-    const handleRemove = (workformId: string) => {
-        const newInputs = inputs.filter((obj) => {
-            return obj.id !== workformId;
-        });
-        setInputs(newInputs);
+    const handleRemove = (workBlockId: string) => {
+        // A bit ugly, I need to convert the top level Record into an array and then reassemble into an object.
+        const newWork = Object.fromEntries(
+            Object.entries(work).filter(([id]) => id !== workBlockId)
+        );
+
+        setWork(newWork);
     };
     return (
         <>
@@ -36,13 +41,14 @@ export default function WorkForm() {
                     ></Button>
                 </div>
 
-                {inputs.map(({ id }) => {
+                {Object.entries(work).map(([id]) => {
                     return (
-                        <WorkInstance
+                        <WorkBlock
+                            setWork={setWork}
                             key={id}
                             id={id}
                             handleRemove={handleRemove}
-                        ></WorkInstance>
+                        ></WorkBlock>
                     );
                 })}
             </div>
