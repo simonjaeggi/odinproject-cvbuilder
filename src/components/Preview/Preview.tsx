ded94d131145278408589ea6e5ba749e55c158a1
simonjaeggi <dev.simon.jaeggi@gmail.com>
2025-02-07 12:34:14 +0100

    created preview with life rendering and download button

--- src/components/Preview/Preview.tsx
+++ src/components/Preview/Preview.tsx
@@ -1,5 +1,44 @@
-export default function Preview() {
-    return <div className="Preview card">
-        
-    </div>;
+import {
+    PDFViewer,
+    PDFDownloadLink,
+} from "@react-pdf/renderer";
+import Button from "../Button/Button";
+import CvLayout from "./CvLayout";
+import "./Preview.css"
+
+const styles = {
+    viewer: {
+        width: "595px",
+        border: "none",
+        height: "842px",
+    },
+}
+
+
+interface PreviewProps{
+    info: Record<string, string>
+    education: Record<string, Record<string, string>>;
+    work: Record<string, Record<string, string>>;
+}
+export default function Preview({info, education, work}:PreviewProps) {
+    return (
+        <div className="Preview card flex-align-cross-center">
+            <PDFViewer showToolbar={false} style={styles.viewer}>
+                <CvLayout info={info} education={education} work={work}></CvLayout>
+            </PDFViewer>
+            <PDFDownloadLink
+                document={<CvLayout info={info} education={education} work={work} />}
+                fileName="MyCV.pdf"
+                style={{ textDecoration: "none" }}
+            >
+                {({ loading }) => (
+                    <Button
+                        label={loading ? "Loading document..." : "Download"}
+                        clickhandler={() => {}}
+                        icon="ri-download-2-line"
+                    />
+                )}
+            </PDFDownloadLink>
+        </div>
+    );
 }
