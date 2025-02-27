4ed9e08b18c36c37f618790fc8be80f766a9044d
simonjaeggi <dev.simon.jaeggi@gmail.com>
2025-02-27 09:06:07 +0100

    removed comment

--- src/components/Form/FormTitle/FormTitle.tsx
+++ src/components/Form/FormTitle/FormTitle.tsx
@@ -1,12 +1,6 @@
 import "./FormTitle.css";
 function FormTitle({ title, icon }: { title: string; icon: string }) {
   return (
-    /**
-     * Good that you are practicing CSS with utility classes!
-     * But just take note that if you are using a CSS framework like Tailwind
-     * or Bootstrap, avoid making your own utility classes but style with normal
-     * CSS instead.
-     */
     <div className="FormTitle flex-row gap-1rem">
       <i className={icon}></i>
       <span>{title}</span>
